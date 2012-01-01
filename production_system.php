a5287b0b7db3d5d47f025fc0e8d6b5fb6b367377
ZininSerge <zinin@xakep.ru>
2012-01-01 20:35:35 +0800

    ~

---
+++ production_system.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package    ProductionSystem
+ * @subpackage Components
+ * components/com_production_system/production_system.php
+ * @license    GNU/GPL
+ */
+
+// No direct access
+defined( '_JEXEC' ) or die( 'Restricted access' );
+
+// Require the base controller
+require_once( JPATH_COMPONENT.DS.'controllers'.DS.'base.php' );
+
+// Require specific controller if requested
+if($controller = JRequest::getWord('controller')) {
+    $path = JPATH_COMPONENT.DS.'controllers'.DS.$controller.'.php';
+    if (file_exists($path)) {
+        require_once $path;
+    } else {
+        $controller = '';
+    }
+}
+
+// Create the controller
+$classname  = $controller.'ProductionSystemController';
+$controller = new $classname();
+
+// Perform the Request task
+$controller->execute( JRequest::getWord( 'task' ) );
+
+// Redirect if set by the controller
+$controller->redirect();
