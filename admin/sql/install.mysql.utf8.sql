a598b7edd5ab7ca4a87d28176979b8d9be971e76
ZininSerge <zinin@xakep.ru>
2012-01-04 19:59:13 +0800

    Initial

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `#__rules`;
+DROP TABLE IF EXISTS `quiz_rules`;
 
 -- --------------------------------------------------------
 
@@ -6,25 +6,7 @@ DROP TABLE IF EXISTS `#__rules`;
 -- Table structure for table `rules`
 --
 
-CREATE TABLE IF NOT EXISTS `#__rules` (
-  `sign_id` int(11) NOT NULL,
-  `weight` float NOT NULL,
-  `body` text NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
-DROP TABLE IF EXISTS `#__signs`;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `signs`
---
-
-CREATE TABLE IF NOT EXISTS `#__signs` (
+CREATE TABLE IF NOT EXISTS `quiz_rules` (
   `name` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,7 +15,24 @@ CREATE TABLE IF NOT EXISTS `#__signs` (
 
 
 
-DROP TABLE IF EXISTS `#__conditions`;
+DROP TABLE IF EXISTS `quiz_signs`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signs`
+--
+
+CREATE TABLE IF NOT EXISTS `quiz_signs` (
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+DROP TABLE IF EXISTS `quiz_conditions`;
 
 -- --------------------------------------------------------
 
@@ -41,7 +40,7 @@ DROP TABLE IF EXISTS `#__conditions`;
 -- Table structure for table `conditions`
 --
 
-CREATE TABLE IF NOT EXISTS `#__conditions` (
+CREATE TABLE IF NOT EXISTS `quiz_conditions` (
   `sign_id` int(11) NOT NULL,
   `rule_id` int(11) NOT NULL,
   `weight` float NOT NULL,
