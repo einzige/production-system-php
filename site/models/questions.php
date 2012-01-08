a95f495bb897bf6537cd4f832347783f39146dc6
ZininSerge <zinin@xakep.ru>
2012-01-08 17:20:35 +0700

    Order questions and answers

--- site/models/questions.php
+++ site/models/questions.php
@@ -7,10 +7,12 @@ class QuizModelQuestions
     public function all() {
         $db = JFactory::getDBO();
 
-        $questions = $db->setQuery("select * from quiz_questions")->loadObjectList();
+        $questions = $db->setQuery("select * from quiz_questions order by position")->loadObjectList();
 
         foreach($questions as &$q) {
-            $q->answers = $db->setQuery("select * from quiz_answers where question_id = $q->id")->loadObjectList();
+          $q->answers = $db->setQuery("select * from quiz_answers 
+                                       where question_id = $q->id 
+                                       ORDER BY position")->loadObjectList();
         }
 
         return $questions;
