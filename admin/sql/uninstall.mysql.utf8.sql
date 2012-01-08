41a1fcb4c512abf55cb28406d612cf75211c570e
ZininSerge <zinin@xakep.ru>
2012-01-08 12:44:01 +0800

    Fixes uninstall script

--- admin/sql/uninstall.mysql.utf8.sql
+++ admin/sql/uninstall.mysql.utf8.sql
@@ -1,1 +1,11 @@
-DROP TABLE IF EXISTS `#__rules`;
+DROP TABLE IF EXISTS `quiz_rules`;
+DROP TABLE IF EXISTS `quiz_signs`;
+DROP TABLE IF EXISTS `quiz_conditions`;
+DROP TABLE IF EXISTS `quiz_results`;
+DROP TABLE IF EXISTS `quiz_rules_results`;
+DROP TABLE IF EXISTS `quiz_questions`;
+DROP TABLE IF EXISTS `quiz_answers`;
+DROP TABLE IF EXISTS `quiz_answers_signs`;
+DROP TABLE IF EXISTS `quiz_rules_signs`;
+DROP TABLE IF EXISTS `quiz_quizzes`;
+DROP TABLE IF EXISTS `quiz_quizzes_results`;
