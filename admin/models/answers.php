953ba57030ad8fc3c9c173863a9886ffa9225cdf
ZininSerge <zinin@xakep.ru>
2012-01-08 17:24:04 +0700

    Sort answers in the admin panel

--- admin/models/answers.php
+++ admin/models/answers.php
@@ -13,7 +13,7 @@ class QuizModelAnswers extends JModelList
     {
         return JFactory::getDBO()->getQuery(true)->select('quiz_answers.id as id,quiz_answers.body as body,quiz_answers.position as position,quiz_questions.body as question')
                                                   ->from('quiz_answers')
-                                                  ->leftJoin('quiz_questions on question_id = quiz_questions.id');
-                                                  //->order('position');
+                                                  ->leftJoin('quiz_questions on question_id = quiz_questions.id')
+                                                  ->order('quiz_questions.position, position');
     }
 }
